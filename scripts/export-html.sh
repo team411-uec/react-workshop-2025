61b8acddd90e04dc874768c8f056f4f1cefa309c
Keita Ito <eguchi@eguchi.cc>
2025-02-18 16:25:40 +0900

    ci: ビルドスクリプト修正

--- scripts/export-html.sh
+++ scripts/export-html.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run --rm -v $PWD:/home/marp/app/ -e LANG=ja_JP.UTF-8 marpteam/marp-cli -I ./ -o ./dist
+docker run --rm -u root -v $PWD:/home/marp/app/ -e LANG=ja_JP.UTF-8 marpteam/marp-cli -I ./ -o ./dist
 
 # npx @marp-team/marp-cli@latest -I ./ -o ./dist
