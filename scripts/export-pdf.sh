47918b68e8c23f2ea8e8e599cb5bd6f5e5262045
Keita Ito <eguchi@eguchi.cc>
2025-02-18 14:27:41 +0900

    chore: init

---
+++ scripts/export-pdf.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker run --rm -v $PWD:/home/marp/app/ -e LANG=ja_JP.UTF-8 marpteam/marp-cli -I ./ -o ./dist --pdf
+
+# npx @marp-team/marp-cli@latest -I ./ -o ./dist --pdf
---
+++ slide-001.md
@@ -0,0 +1,7 @@
+---
+marp: true
+paginate: true
+theme: react-workshop
+---
+
+# Slide 1
